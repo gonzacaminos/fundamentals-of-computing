79eed21c740f9fdf7d6d4606b4a7e548ef8695b5
Gonza <5827300+gonzalobonini@users.noreply.github.com>
2020-04-30 17:48:55 +0800

    Finished main 2048 specification clas

--- Principles of Computing (Part 1 & 2)/Week 2/test_2048.py
+++ Principles of Computing (Part 1 & 2)/Week 2/test_2048.py
@@ -14,7 +14,17 @@ def run_suite(twentyfortyeight):
     
     HEIGHT = 4 
     WIDTH = 6
+    NUM_CELLS = HEIGHT*WIDTH
+    DIR_UP = str([[0,0], [0,1], [0,2], [0,3], [0,4], [0,5]])
+    DIR_LEFT = str([[0,0], [1,0], [2,0], [3,0]])
+    DIR_DOWN = str([[3,0], [3,1], [3,2], [3,3], [3,4], [3,5]])
+    DIR_RIGHT = str([[0,5], [1,5], [2,5], [3,5]])
 
+    # Directions, DO NOT MODIFY
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
     # create a game
     twenty48 = twentyfortyeight(HEIGHT,WIDTH)
     
@@ -24,6 +34,34 @@ def run_suite(twentyfortyeight):
     
     # test the initial configuration of the board using the str method
     suite.run_test(str(twenty48), str(EXAMPLE_GRID), "Test #0: init")
+    twenty48.new_tile()
+    #suite.run_test(str(twenty48.get_tile(1,1)), str(4), "Test #1: set/get_tile")
+    twenty48.new_tile()
+    #suite.run_test(str(twenty48.get_tile(3,3)), str(2), "Test #1: set/get_tile")
+    suite.run_test(str(len(twenty48.get_empty_cells())), str(NUM_CELLS -4), "Test #2: get_empty_cells") # Should return NUM_CELLS - 2 
+    #twenty48.set_tile(2,3, 2)
+    twenty48.new_tile()
+    suite.run_test(str(len(twenty48.get_empty_cells())), str(NUM_CELLS -5), "Test #2: get_empty_cells") # Should return NUM_CELLS - 3 
+    #suite.run_test(str(twenty48.get_random_empty_cell()), str("Random"), "Test #3: get_random_empty_cell") 
+    suite.run_test(str(twenty48.DIRECTION[UP]), DIR_UP, "Test #4: UP direction") 
+    suite.run_test(str(twenty48.DIRECTION[LEFT]), DIR_LEFT, "Test #4: LEFT direction") 
+    suite.run_test(str(twenty48.DIRECTION[DOWN]), DIR_DOWN, "Test #4: DOWN direction") 
+    suite.run_test(str(twenty48.DIRECTION[RIGHT]), DIR_RIGHT, "Test #4: RIGHT direction") 
+    twenty48.print_grid_snapshot()
+    suite.run_test(str(twenty48.move(UP)), "Something", "Test #5: Move Up") 
+    twenty48.print_grid_snapshot()
+    suite.run_test(str(twenty48.move(DOWN)), "Something", "Test #5: Move Down") 
+    twenty48.print_grid_snapshot()
+    suite.run_test(str(twenty48.move(LEFT)), "Something", "Test #5: Move Left") 
+    twenty48.print_grid_snapshot()
+
+
+    suite.run_test(str(twenty48.reset()), str(twenty48), "Test #4: reset") 
+
+    #for _dummy_i in range(22):
+    #    print twenty48.new_tile()
+    
+    #suite.run_test(str(twenty48), str(EXAMPLE_GRID), "Test #0: init")
 
     # report number of tests and failures
     suite.report_results()
--- Principles of Computing (Part 1 & 2)/Week 2/twenty48.py
+++ Principles of Computing (Part 1 & 2)/Week 2/twenty48.py
@@ -4,6 +4,7 @@ Clone of 2048 game.
 
 #import poc_2048_gui
 import test_2048 as test_suite
+import random
 # Directions, DO NOT MODIFY
 UP = 1
 DOWN = 2
@@ -16,6 +17,9 @@ OFFSETS = {UP: (1, 0),
            DOWN: (-1, 0),
            LEFT: (0, 1),
            RIGHT: (0, -1)}
+
+
+
 
 
 def next_available(line):
@@ -66,6 +70,11 @@ class TwentyFortyEight:
         self.grid_height = grid_height
         self.grid_width  = grid_width
         self.grid = []
+        self.DIRECTION = {UP:   self.traverse_grid((0, 0), (0, 1), self.grid_width),
+                          LEFT: self.traverse_grid((0,0), (1,0), self.grid_height),
+                          DOWN: self.traverse_grid((self.grid_height-1,0), (0,1), self.grid_width),
+                          RIGHT:self.traverse_grid((0,self.grid_width-1), (1,0), self.grid_height)}
+
         self.reset()
 
     def reset(self):
@@ -75,8 +84,11 @@ class TwentyFortyEight:
         """
         self.grid = [[0 + 0 for _dummy_col in range(self.grid_width)]
                             for _dummy_row in range(self.grid_height)]
-        # Add new_tile x 2
-        
+        self.new_tile()
+        self.new_tile()
+
+        return self.grid
+    
     def __str__(self):
         """
         Return a string representation of the grid for debugging.
@@ -84,27 +96,104 @@ class TwentyFortyEight:
         # replace with your code
         return str(self.grid)
 
+    
+
     def get_grid_height(self):
         """
         Get the height of the board.
         """
         # replace with your code
-        return 0
+        return self.grid_height
 
     def get_grid_width(self):
         """
         Get the width of the board.
         """
         # replace with your code
-        return 0
+        return self.grid_width
+    def traverse_grid(self, start_cell, direction, num_steps):
+        """
+        Function that iterates through the cells in a grid
+        in a linear direction
+        
+        Both start_cell is a tuple(row, col) denoting the
+        starting cell
+        
+        direction is a tuple that contains difference between
+        consecutive cells in the traversal
+
+        Returns a list with the processed grid cells
+        """
+        _processed_grid = []
+        _values = []
+        for step in range(num_steps):
+            row = start_cell[0] + step * direction[0]
+            col = start_cell[1] + step * direction[1]
+            _processed_grid.append([row,col])
+            
+         
+        return _processed_grid
+
+    def get_grid_values(self, start_cell, direction, num_steps): 
+        """
+        Function that iterates on the grid on a linear direction
+        Gets the value of each tile and returns a list
+        Uses the same parameters that the traverse_grid method
+        """
+        _grid = self.traverse_grid(start_cell,direction, num_steps)
+        _values = []
+        for _dummy_x in _grid:
+            _value = self.get_tile(_dummy_x[0],_dummy_x[1])
+            _values.append(_value)
+        return _values
+
+    def set_grid_values(self, start_cell, direction, num_steps, list): 
+        """
+        Function that iterates on the grid on a linear direction
+        Sets the value of each tile and returns a list
+        Uses the same parameters that the traverse_grid method
+        """
+        _grid = self.traverse_grid(start_cell,direction, num_steps)
+        for _index, _value in enumerate(_grid):
+            #print list[_index], _value[0], _value[1]
+            self.set_tile(_value[0],_value[1], list[_index])
 
     def move(self, direction):
         """
         Move all tiles in the given direction and add
         a new tile if any tiles moved.
         """
-        # replace with your code
-        pass
+        print "MOVING >>>"
+        _this_direction = self.DIRECTION[direction]
+        _offset = OFFSETS[direction]
+        _steps =  self.grid_height  if direction == UP or direction == DOWN else self.grid_width 
+
+        for _dummy_x in _this_direction:
+            _temp_list = self.get_grid_values(_dummy_x,_offset, _steps) # Gets the grid values
+            _temp_list = merge(_temp_list) # Merges the column/row
+            self.set_grid_values(_dummy_x,_offset, _steps, _temp_list) # Sets the values to the merged list 
+
+    
+    def print_grid_snapshot(self):
+         print "SNAPSHOT:"
+         for row in range(self.grid_height):
+            print self.grid[row]
+
+    def get_empty_cells(self):
+        _cells = []
+        for _row in range(self.grid_height):
+            for _col in range(self.grid_width):
+                if(self.grid[_row][_col] == 0):
+                    _cells.append([_row,_col])
+                   
+        return _cells
+
+    def get_random_empty_cell(self):
+        _cells = self.get_empty_cells()
+        if(self.get_empty_cells()):
+            return random.choice(_cells)
+        else:
+            return False
 
     def new_tile(self):
         """
@@ -112,24 +201,27 @@ class TwentyFortyEight:
         square.  The tile should be 2 90% of the time and
         4 10% of the time.
         """
-        # replace with your code
-        pass
+        _number = 2 if random.random() < .9 else 4
+        _empty_cell = self.get_random_empty_cell()
+        
+        if(_empty_cell):
+            self.set_tile(_empty_cell[0],_empty_cell[1],_number)    
+        else: 
+            return "No more empty"     
 
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
         """
-        # replace with your code
-        pass
+        self.grid[row][col] = value
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """
-        # replace with your code
-        return 0
+        return self.grid[row][col]
     
 
-twenty48 = TwentyFortyEight(4,6)
+#twenty48 = TwentyFortyEight(4,6)
 test_suite.run_suite(TwentyFortyEight)
 #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
