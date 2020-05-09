edc3781e6563b3ad76e02110c3ea173ce0816aff
Gonza <5827300+gonzalobonini@users.noreply.github.com>
2020-05-09 22:26:13 +0800

    Finished Montecarlo implementation

--- POC/Week3/mini_project/tictactoe.py
+++ POC/Week3/mini_project/tictactoe.py
@@ -9,7 +9,7 @@ import tictactoe_board as provided
 # Constants for Monte Carlo simulator
 # You may change the values of these constants as desired, but
 #  do not change their names.
-NTRIALS = 10000  # Number of trials to run
+NTRIALS = 300  # Number of trials to run
 SCORE_CURRENT = 1.0  # Score for squares played by the current player
 SCORE_OTHER = 1.0  # Score for squares played by the other player
 
@@ -25,26 +25,22 @@ def mc_trial(board, player):
         _square = random.choice(board.get_empty_squares())    
         board.move(_square[0], _square[1], _player)   
         _player = provided.switch_player(_player)
-    #print board.check_win()
 
 
 def mc_update_scores(scores, board, player):
     """
     Updates the scores grids by scoring a given board and a given player
     """
-    #board.check_win()
-    if(board.check_win() == provided.DRAW):
+    _board_win = board.check_win()
+    if(_board_win  == provided.DRAW):
         scores = create_empty_sq_list(len(scores))
         return scores
 
-    _score_current = SCORE_CURRENT * 1 if board.check_win() == player else SCORE_CURRENT * -1
-    _score_other = SCORE_OTHER * 1 if board.check_win() is not player else SCORE_OTHER * -1
-
-    #print board.check_win(), player, _score_current, _score_other
+    _score_current = SCORE_CURRENT * 1 if _board_win == player else SCORE_CURRENT * -1
+    _score_other = SCORE_OTHER * 1 if _board_win is not player else SCORE_OTHER * -1
 
     for _sq_x in range(len(scores)):
         for _sq_y in range(len(scores[_sq_x])):
-            #print board.square(_sq_x ,_sq_y)
             if(board.square(_sq_x ,_sq_y) == player):
                 scores[_sq_x][_sq_y] += _score_current
             elif(board.square(_sq_x ,_sq_y) is not 1):
@@ -57,21 +53,24 @@ def get_best_move(board, scores):
     Gets the best possible move for a given board by using a scores grid
     """
     _empty_sq = board.get_empty_squares()
-    _max_value = 0
-    _best_move = ()
-    if(len(_empty_sq) > 0):
-        for _dumm_sq in _empty_sq:
-            _sq_score = scores[_dumm_sq[0]][_dumm_sq[1]]
-            if(_sq_score > _max_value):
-                _max_value = _sq_score
-                _best_move = _dumm_sq
+    _first_tile = _empty_sq[0]
+    _max_value = scores[_first_tile[0]][_first_tile[1]]
+   
+    _best_move = _first_tile
+    
+    for _dumm_sq in _empty_sq:
+        _sq_score = scores[_dumm_sq[0]][_dumm_sq[1]]
+        if(_sq_score > _max_value):
+            _max_value = _sq_score
+            _best_move = _dumm_sq    
+
     return _best_move
 
 def create_empty_sq_list(dimension):
     """
     Create an empty (filled with zeroes) square list 
     """
-    return [([0] * dimension) for i in range(dimension)]
+    return [([0] * dimension) for _dummy_i in range(dimension)]
 
 def mc_move(board, player, trials):
     """
@@ -83,17 +82,14 @@ def mc_move(board, player, trials):
         _board = board.clone() # New board based on the given board
         mc_trial(_board,player)
         mc_update_scores(_scores, _board,player)
+    
         
-
     return get_best_move(board, _scores)
    
-
-
-
 
 # Test game with the console or the GUI.  Uncomment whichever
 # you prefer.  Both should be commented out when you submit
 # for testing to save time.
 
-#provided.play_game(mc_move, NTRIALS, False)
-# poc_ttt_gui.run_gui(3, provided.PLAYERX, mc_move, NTRIALS, False)
+provided.play_game(mc_move, NTRIALS, False)
+#poc_ttt_gui.run_gui(3, provided.PLAYERX, mc_move, NTRIALS, False)
