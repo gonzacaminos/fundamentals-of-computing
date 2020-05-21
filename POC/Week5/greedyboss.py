64236fc86b77a5f6912597d07c73fa734ba5e288
Gonza <5827300+gonzalobonini@users.noreply.github.com>
2020-05-21 18:37:01 +0800

    Improved version, working with LOGLOG

--- POC/Week5/greedyboss.py
+++ POC/Week5/greedyboss.py
@@ -26,39 +26,43 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type = STANDARD):
     earnings = 0
     salary = 100
     bribe = 1000
-    current_day = 1
+    current_day = 0
     money = 0
     # define  list consisting of days vs. total salary earned for analysis
     days_vs_earnings = []
-    days_vs_earnings.append((0,0))
     # Each iteration of this while loop simulates one bribe
     while current_day <= days_in_simulation:
-        earnings += salary
-        money += salary
-        print "Day:", current_day, "Money:", money, "Earnings:", earnings, "Salary:", salary, "Bribe:",bribe, int(math.floor(earnings/bribe))
         
-        for i in range(int(money/bribe)): 
-            # Bribe day!
-            if(plot_type == STANDARD):
-                days_vs_earnings.append((current_day,earnings))
-            else:
-                days_vs_earnings.append((math.log(current_day), math.log(earnings)))
-            money -= bribe
-            bribe += bribe_cost_increment
-            salary += 100
-            
         # update list with days vs total salary earned
         # use plot_type to control whether regular or log/log plot
+        
+        if(plot_type == STANDARD):
+            days_vs_earnings.append((current_day,earnings))
+        else:
+            days_vs_earnings.append([math.log(current_day), math.log(earnings)])
+   
+       
         # check whether we have enough money to bribe without waiting
+        
+        if(money>=bribe):
+            days_to_skip = 0
+        else:            
+            days_to_skip = math.ceil((bribe - money)/salary)
+            
         # advance current_day to day of next bribe (DO NOT INCREMENT BY ONE DAY)
-        days_to_skip = math.ceil((bribe - money)/salary)
+
         current_day += days_to_skip
-        # We take one salary out of the salary, it will be added later
-        skipped_salary = salary * (days_to_skip - 1)
+        
+        skipped_salary = salary * days_to_skip
         
         earnings += skipped_salary
         money += skipped_salary
+        money -= bribe
+        bribe += bribe_cost_increment
+        salary += 100
+        
         # update state of simulation to reflect bribe
+        
     return days_vs_earnings
 
 
@@ -77,14 +81,16 @@ def run_simulations():
                          ["Bribe increment = 0", "Bribe increment = 500",
                           "Bribe increment = 1000", "Bribe increment = 2000"])
 
+
+
 run_simulations()
 
 
 
-#print greedy_boss(35, 100)
+print greedy_boss(35, 100)
 # should print [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700)]
 
-print greedy_boss(35, 0)
+#print greedy_boss(35, 0)
 # should print [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000), (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300), (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35, 16900)]
 
 
